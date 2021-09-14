35a8ccbbcfb5898f9b1c25268206d9b7ccb811da
Bruno Castrillon <brunocastrillon@gmail.com>
2021-09-14 11:37:27 -0400

    Unit Test - V1
    - Foram criados dois testes unitários para a construção das duas entidades. neste etapa, ambas estão desvinculadas

---
+++ Bookstore/Bookstore.Teste.Unit/Core/Entities/BookConstructor.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore.Core.Entities;
+using Xunit;
+
+namespace Bookstore.Teste.Unit.Core.Entities
+{
+    public class BookConstructor
+    {
+        private string _testTitle = "Clean Architecture";
+        private Book _testBook = null;
+
+        public Book CreateBook()
+        {
+            return new Book(_testTitle);
+        }
+
+        [Fact]
+        public void InitializeTitle()
+        {
+            _testBook = CreateBook();
+            Assert.Equal(_testTitle, _testBook.Title);
+        }
+    }
+}
