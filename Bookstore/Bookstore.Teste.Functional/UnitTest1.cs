151a6e75254c8144d929c6c2c3eeaeac3504fea6
Bruno Castrillon <brunocastrillon@gmail.com>
2021-09-14 09:11:40 -0400

    Estrutura inicial do projeto

---
+++ Bookstore/Bookstore.Teste.Functional/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bookstore.Teste.Functional
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Bookstore/Bookstore.Teste.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bookstore.Teste.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
