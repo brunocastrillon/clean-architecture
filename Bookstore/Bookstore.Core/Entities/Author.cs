a9e87e1ddb9f20c82a3c374586eb86f65fa22fe4
Bruno Castrillon <brunocastrillon@gmail.com>
2021-09-14 10:47:59 -0400

    Core-Entities
    Entidades criadas de forma independentes para serem testadas unitariamente neste primeira etapa do projeto

---
+++ Bookstore/Bookstore.Core/Entities/Author.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore.SharedKernel;
+using Bookstore.SharedKernel.Interfaces;
+
+namespace Bookstore.Core.Entities
+{
+    public class Author : BaseEntity, IAggregateRoot
+    {
+        public string Name { get; set; }
+
+        public Author(string name)
+        {
+            Name = name;
+        }
+    }
+}
