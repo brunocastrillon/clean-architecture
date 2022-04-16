168c732afe2d8de5fa0f245440b6316a9ed4c2dc
Bruno Castrillon <brunocastrillon@gmail.com>
2022-04-16 16:27:32 -0400

    refactor-wip
    - criação de um projeto de teste de integração para testar a camada de infra
    - remoção de projetos obsoletos

---
+++ Bookstore/Bookstore.Test.Integration/Data/Category/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bookstore.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookstore.Test.Integration.Data.Category
+{
+    public class CategoryRepository
+    {
+        protected ApplicationDbContext _applicationDbContext;
+
+        protected static DbContextOptions<ApplicationDbContext> DbContextOptions()
+        {
+            var serviceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            builder.UseInMemoryDatabase("Bookstore").UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        protected Infra.Data.Repository.CategoryRepository GetRepository()
+        {
+            var options = DbContextOptions();
+
+            _applicationDbContext = new ApplicationDbContext(options);
+
+            return new Infra.Data.Repository.CategoryRepository(_applicationDbContext);
+        }
+    }
+}
