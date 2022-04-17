abd2ebe89626d2547fdf921230e3b8772f282c08
Bruno Castrillon <brunocastrillon@gmail.com>
2022-04-17 16:36:39 -0400

    refactor-wip
    - criado uma nova camada de IoC
    - foram adicionados configurações para os serviços e repositórios criados
    - foi adicionado uma configuração para o sqlite

---
+++ Bookstore/Bookstore.Infra.IoC/Repositories.cs
@@ -0,0 +1,18 @@
+﻿using Bookstore.Core.Domain.Contracts;
+using Bookstore.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookstore.Infra.IoC
+{
+    public static class Repositories
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            return services;
+        }
+    }
+}
